e0efe7f9d4a2c275e3f61d7b101fdd56fe3e43e9
Gordon Wilford <g.r.wilford@gmail.com>
2020-03-12 15:13:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,16 +1,16 @@
 
 
-# def find_element_index(array, value_to_find)
-#   count = 0
-#   while count < array.length do
-#     if array[count] == value_to_find
-#       return count
-#       break
-#     else
-#       count += 1
-#     end 
-#   end
-# end
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < array.length do
+    if array[count] == value_to_find
+      return count
+      break
+    else
+      count += 1
+    end 
+  end
+end
 
 # def find_max_value(array)
 # for i in array do 
