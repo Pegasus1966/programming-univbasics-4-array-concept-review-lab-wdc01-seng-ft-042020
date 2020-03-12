ac9aa71a613fa46a302bda8b71afefacdb86ebaf
Gordon Wilford <g.r.wilford@gmail.com>
2020-03-12 15:15:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,19 +12,19 @@ def find_element_index(array, value_to_find)
   end
 end
 
-def find_max_value(array)
-for i in array do 
-  if array[0] > array[1]
-    array.delete_at(1)
-  else
-    array.delete_at(0)
-  end
-  end
-  if array[0] > array[1]
-    array[0]
-  else array[1]
-  end
-end
+# def find_max_value(array)
+# for i in array do 
+#   if array[0] > array[1]
+#     array.delete_at(1)
+#   else
+#     array.delete_at(0)
+#   end
+#   end
+#   if array[0] > array[1]
+#     array[0]
+#   else array[1]
+#   end
+# end
 
 def find_min_value(array)
   for i in array do
